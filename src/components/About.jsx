0c787c85ec2877d1c5ade00b4c257a6c9155ba63
ArnoVanSteenbergen03 <arno.vansteenbergen@student.arteveldehs.be>
2025-09-11 16:31:58 +0200

    added recipes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ function About() {
     <div>
       <h1>About</h1>
       
-      <p>This is a simple recipe book app built with React.</p>
+      <p>This is a simple recipe book app built with React and Vite with a Firebase databse.</p>
       <p>It allows users to browse and search for recipes, as well as learn about different spices.</p>
     </div>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 function Home() {
+  const [publicRecipes, setPublicRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchPublicRecipes = async () => {
+      const q = query(collection(db, "recipes"), where("public", "==", true));
+      const querySnapshot = await getDocs(q);
+      const recipesList = [];
+      querySnapshot.forEach((doc) => {
+        recipesList.push({ id: doc.id, ...doc.data() });
+      });
+      setPublicRecipes(recipesList);
+    };
+    fetchPublicRecipes();
+  }, []);
+
   return (
     <section className="section" id="home">
       <div>Welcome to the Recipe Book</div>
+
+      <div>
+        <p>Discover a world of culinary delights with our Recipe Book app!</p>
+        <h2>Public Recipes</h2>
+        <ul>
+          {publicRecipes.map((recipe) => (
+            <li key={recipe.id}>
+              {recipe.name}:{" "}
+              {recipe.ingredients && recipe.ingredients.join(", ")}
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 }
